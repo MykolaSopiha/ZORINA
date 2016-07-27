10b3cf9a56952c0c5611b2e5a23f5a2d7fa8a512
Sopiha Mykola <sopiha.mv@yandex.ru>
2016-07-28 00:37:21 +0300

    first commit

---
+++ script/js.js
@@ -0,0 +1,30 @@
+// Scroll Up!
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollup').fadeIn();
+		} else {
+			$('.scrollup').fadeOut();
+		}
+	});
+	$('.scrollup').click(function(){
+		$("html, body").animate({ scrollTop: 0 }, 600);
+		return false;
+	});
+});
+
+$(document).ready(function(){
+	var touch = $('#touch-menu');
+    var menu = $('.nav');
+ 
+    $(touch).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+    $(window).resize(function(){
+        var wid = $(window).width();
+        if(wid > 760 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+});
